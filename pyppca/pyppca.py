9f006c2909291a023627f5b888c018b57dcbe623
Ludvig Hult <ludvig.hult@gmail.com>
2019-06-20 12:20:53 +0200

    Tidying if clauses, removed unused variable, added attribution in docstring

--- pyppca/pyppca.py
+++ pyppca/pyppca.py
@@ -7,6 +7,8 @@ Modifications made in order to support high-dimensional matrices
 Sheridan Beckwith Green
 sheridan.green@yale.edu
 Sept 2018
+
+Updates by Ludvig Hult in 2019
 """
 
 import numpy as np
@@ -18,7 +20,8 @@ from numpy import identity as eye
 from numpy import trace as tr
 from scipy.linalg import orth
 
-def ppca(Y,d,dia):
+
+def ppca(Y, d, dia):
     """
     Implements probabilistic PCA for data with missing values,
     using a factorizing distribution over hidden states and hidden observations.
@@ -37,70 +40,69 @@ def ppca(Y,d,dia):
 
         Based on MATLAB code from J.J. VerBeek, 2006. http://lear.inrialpes.fr/~verbeek
     """
-    N, D      = shape(Y) #N observations in D dimensions (i.e. D is number of features, N is samples)
-    threshold = 1E-4 #minimal relative change in objective function to continue
-    hidden    = isnan(Y)
-    missing   = hidden.sum()
+    N, D = shape(Y)  # N observations in D dimensions (i.e. D is number of features, N is samples)
+    threshold = 1E-4  # minimal relative change in objective function to continue
+    hidden = isnan(Y)
+    missing = hidden.sum()
 
-    if(missing > 0):
+    if missing > 0:
         M = nanmean(Y, axis=0)
     else:
         M = average(Y, axis=0)
 
-    Ye = Y - repmat(M,N,1)
+    Ye = Y - repmat(M, N, 1)
 
-    if(missing > 0):
+    if missing > 0:
         Ye[hidden] = 0
 
-    #initialize
-    C = normal(loc=0.0, scale=1.0, size=(D,d))
+    # initialize
+    C = normal(loc=0.0, scale=1.0, size=(D, d))
     CtC = C.T @ C
-    X   = Ye @ C @ inv(CtC)
+    X = Ye @ C @ inv(CtC)
     recon = X @ C.T
     recon[hidden] = 0
-    ss = np.sum((recon-Ye)**2) / (N*D - missing)
+    ss = np.sum((recon - Ye) ** 2) / (N * D - missing)
 
     count = 1
     old = np.inf
 
-    #EM Iterations
-    while(count):
-        Sx = inv(eye(d) + CtC/ss) #E-step, covariances
+    # EM Iterations
+    while (count):
+        Sx = inv(eye(d) + CtC / ss)  # E-step, covariances
         ss_old = ss
-        if(missing > 0):
-            proj = X @C.T
+        if missing > 0:
+            proj = X @ C.T
             Ye[hidden] = proj[hidden]
-        
-        X = Ye @ C @ Sx / ss #E-step: expected values
 
-        SumXtX = X.T @ X #M-step
-        C = Ye.T @ X @(SumXtX + N*Sx).T @ inv(((SumXtX + N*Sx) @ (SumXtX + N*Sx).T))
+        X = Ye @ C @ Sx / ss  # E-step: expected values
+
+        SumXtX = X.T @ X  # M-step
+        C = Ye.T @ X @ (SumXtX + N * Sx).T @ inv(((SumXtX + N * Sx) @ (SumXtX + N * Sx).T))
         CtC = C.T @ C
-        ss = (np.sum((X @ C.T - Ye)**2) + N*np.sum(CtC*Sx) + missing*ss_old) / (N*D)
-        #transform Sx determinant into numpy longdouble in order to deal with high dimensionality
-        Sx_det = np.min(Sx).astype(np.longdouble)**shape(Sx)[0] * det(Sx / np.min(Sx))
-        objective = N*D + N*(D*log(ss)+tr(Sx)-log(Sx_det)) + tr(SumXtX) - missing*log(ss_old)
-        
-        rel_ch = np.abs( 1 - objective / old )
+        ss = (np.sum((X @ C.T - Ye) ** 2) + N * np.sum(CtC * Sx) + missing * ss_old) / (N * D)
+        # transform Sx determinant into numpy longdouble in order to deal with high dimensionality
+        Sx_det = np.min(Sx).astype(np.longdouble) ** shape(Sx)[0] * det(Sx / np.min(Sx))
+        objective = N * D + N * (D * log(ss) + tr(Sx) - log(Sx_det)) + tr(SumXtX) - missing * log(ss_old)
+
+        rel_ch = np.abs(1 - objective / old)
         old = objective
-        
+
         count = count + 1
-        if( rel_ch < threshold and count > 5 ):
+        if rel_ch < threshold and count > 5:
             count = 0
-        if(dia == True):
-            print('Objective: %.2f, Relative Change %.5f' %(objective, rel_ch))
-    
+        if dia:
+            print('Objective: %.2f, Relative Change %.5f' % (objective, rel_ch))
+
     C = orth(C)
-    covM = cov( (Ye @ C).T)
-    vals, vecs = eig( covM )
+    covM = cov((Ye @ C).T)
+    vals, vecs = eig(covM)
     ordr = np.argsort(vals)[::-1]
-    vals = vals[ordr]
-    vecs = vecs[:,ordr]
-    
+    vecs = vecs[:, ordr]
+
     C = C @ vecs
     X = Ye @ C
-    
-    #add data mean to expected complete data
-    Ye = Ye + repmat(M,N,1)
+
+    # add data mean to expected complete data
+    Ye = Ye + repmat(M, N, 1)
 
     return C, ss, M, X, Ye
