0cc591414a681cf2e823251970549f023be0bda6
Ludvig Hult <ludvig.hult@gmail.com>
2019-06-20 12:09:24 +0200

    Fixed: 1) removed unused imports 2) changed matmul to @ in accordance with PEP465, so that drops support for python pre 3.5 3) change float128 to longdouble, which should have same precision, but wider platform support 4) updated doctring so return value order is correct with actual code

--- pyppca/pyppca.py
+++ pyppca/pyppca.py
@@ -10,8 +10,7 @@ Sept 2018
 """
 
 import numpy as np
-from numpy import shape, isnan, nanmean, average, zeros, log, cov
-from numpy import matmul as mm
+from numpy import shape, isnan, nanmean, average, log, cov
 from numpy.matlib import repmat
 from numpy.random import normal
 from numpy.linalg import inv, det, eig
@@ -55,9 +54,9 @@ def ppca(Y,d,dia):
 
     #initialize
     C = normal(loc=0.0, scale=1.0, size=(D,d))
-    CtC = mm(C.T, C)
-    X   = mm(mm(Ye, C), inv(CtC))
-    recon = mm(X, C.T)
+    CtC = C.T @ C
+    X   = Ye @ C @ inv(CtC)
+    recon = X @ C.T
     recon[hidden] = 0
     ss = np.sum((recon-Ye)**2) / (N*D - missing)
 
@@ -69,17 +68,17 @@ def ppca(Y,d,dia):
         Sx = inv(eye(d) + CtC/ss) #E-step, covariances
         ss_old = ss
         if(missing > 0):
-            proj = mm(X,C.T)
+            proj = X @C.T
             Ye[hidden] = proj[hidden]
         
-        X = mm(mm(Ye, C), Sx/ss) #E-step: expected values
+        X = Ye @ C @ Sx / ss #E-step: expected values
 
-        SumXtX = mm(X.T, X) #M-step
-        C = mm(mm(mm(Ye.T, X),(SumXtX + N*Sx).T), inv(mm((SumXtX + N*Sx), (SumXtX + N*Sx).T)))
-        CtC = mm(C.T, C)
-        ss = (np.sum((mm(X, C.T) - Ye)**2) + N*np.sum(CtC*Sx) + missing*ss_old) / (N*D)
-        #transform Sx determinant into numpy float128 in order to deal with high dimensionality
-        Sx_det = np.min(Sx).astype(np.float128)**shape(Sx)[0] * det(Sx / np.min(Sx))
+        SumXtX = X.T @ X #M-step
+        C = Ye.T @ X @(SumXtX + N*Sx).T @ inv(((SumXtX + N*Sx) @ (SumXtX + N*Sx).T))
+        CtC = C.T @ C
+        ss = (np.sum((X @ C.T - Ye)**2) + N*np.sum(CtC*Sx) + missing*ss_old) / (N*D)
+        #transform Sx determinant into numpy longdouble in order to deal with high dimensionality
+        Sx_det = np.min(Sx).astype(np.longdouble)**shape(Sx)[0] * det(Sx / np.min(Sx))
         objective = N*D + N*(D*log(ss)+tr(Sx)-log(Sx_det)) + tr(SumXtX) - missing*log(ss_old)
         
         rel_ch = np.abs( 1 - objective / old )
@@ -92,14 +91,14 @@ def ppca(Y,d,dia):
             print('Objective: %.2f, Relative Change %.5f' %(objective, rel_ch))
     
     C = orth(C)
-    covM = cov(mm(Ye, C).T)
+    covM = cov( (Ye @ C).T)
     vals, vecs = eig( covM )
     ordr = np.argsort(vals)[::-1]
     vals = vals[ordr]
     vecs = vecs[:,ordr]
     
-    C = mm(C, vecs)
-    X = mm(Ye, C)
+    C = C @ vecs
+    X = Ye @ C
     
     #add data mean to expected complete data
     Ye = Ye + repmat(M,N,1)
